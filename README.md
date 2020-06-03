57b96c187e850bb05f51039ea77dd55654f4a9ea
shitab <shitab.mailbox01@gmail.com>
2020-06-03 22:43:55 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shitabs_flutter_sandbox
+ Sandbox for Flutter/Dart Practice
---
+++ lib/myclasses/UtilClass.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UtilClass {
+
+  // -------------- Device -----------------------
+  static void setOrientationPortraitUpOnly() {
+    SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp]
+    );
+  }
+  static setOrientationPortraitDownOnly() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitDown]
+    );
+  }
+  static setOrientationLandscapeLeftOnly() {
+      SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft]
+      );
+  }
+  static setOrientationLandscapeRightOnly() {
+        SystemChrome.setPreferredOrientations(
+            [DeviceOrientation.landscapeRight]
+        );
+      }
+  static setOrientationLandscapeLeftPortraitUp() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.landscapeLeft]
+    );
+  }
+  static setOrientationAllSupported() {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown, DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]
+    );
+  }
+
+  static double getScreenHeight(BuildContext context){
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double getScreenWidth(BuildContext context){
+    return MediaQuery.of(context).size.width;
+  }
+
+  // ------------ Page --------------------
+  static void setNotificationBarColorOnly(Color color, Brightness iconBrightness) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: color,
+      statusBarIconBrightness: iconBrightness,
+    ));
+  }
+  static void setNavigationBarColorOnly(Color color, Brightness navBrightness) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: color,
+      systemNavigationBarIconBrightness: navBrightness,
+    ));
+  }
+  static void setSystemStyle(Color statusBarColor, Color navigationBarColor, Brightness iconBrightness, Brightness navBrightness) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: statusBarColor,
+      systemNavigationBarColor: navigationBarColor,
+      statusBarIconBrightness: iconBrightness,
+      systemNavigationBarIconBrightness: navBrightness,
+    ));
+  }
+
+
+
+
+}
